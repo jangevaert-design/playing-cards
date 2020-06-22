06db0c30a2c19e780e4a5ccc870a9327c779e0fc
Jan Gevaert <jan.gevaert@gmail.com>
2020-06-22 12:02:44 -0600

    finalized the project

--- src/edu/cnm/deepdive/Suit.java
+++ src/edu/cnm/deepdive/Suit.java
@@ -1,28 +1,42 @@
 package edu.cnm.deepdive;
 
 /**
- * Encaptulates the suits of standard playing cards. Each instance can return
- { * (using the {@link #symbol()} method) the Unicode symbol for tht suit.
- } */
+ * Encaptulates the suits of standard playing cards. Each instance can return { * (using the {@link
+ * #symbol()} method) the Unicode symbol for tht suit. }
+ */
 public enum Suit {
 
-   CLUBS, DIAMONDS, HEARTS, SPADES;
+  CLUBS(Color.BLACK),
+  DIAMONDS(Color.RED),
+  HEARTS(Color.RED),
+  SPADES(Color.BLACK);
+
+  private final Color color;
+
+  private Suit(Color color) {
+    this.color = color;
+  }
 
   /**
    * Returns the Unicode symbol for the current instance.
+   *
    * @return One of the four symbols: ♡, 	♢, ♠, ♣;
    */
-   public char symbol() {
-     switch (this) {
-       case CLUBS:
-         return '\u2663';
-       case DIAMONDS:
-         return '\u2662';
-       case HEARTS:
-         return '\u2661';
-       default:
-         return '\u2660';
+  public char symbol() {
+    switch (this) {
+      case CLUBS:
+        return '\u2663';
+      case DIAMONDS:
+        return '\u2662';
+      case HEARTS:
+        return '\u2661';
+      default:
+        return '\u2660';
 
-     }
-   }
+    }
+  }
+
+  public Color getColor() {
+    return color;
+  }
 }
